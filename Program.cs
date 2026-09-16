bc98f12b8c4137737b87c3f0a3b5b6bf237b8f9c
Juan <lardevpro@gmail.com>
2026-09-16 10:41:22 +0200

    commit

--- Program.cs
+++ Program.cs
@@ -17,3 +17,31 @@ else
 {
     Console.WriteLine("No es mayor de edad.");
 }
+    //*Ejercicio 2 — 
+    // Número positivo 
+    // Pide un número entero.Comprueba si es mayor que 0.
+
+Console.Write("Introduce un número entero:"); 
+int? numero = int.Parse(Console.ReadLine()!);
+
+if (numero > 0)
+{
+    Console.WriteLine("El número es positivo.");
+}
+else
+{
+    Console.WriteLine("El número no es positivo.");
+}
+
+    // Ejercicio 4 — Temperatura
+    // Pide una temperatura.
+    // Si es superior a 30 grados, muestra:
+    // Hace calor
+
+Console.Write("Introduce la temperatura:");
+int? temperatura = int.Parse(Console.ReadLine()!);
+
+if (temperatura > 30)
+{
+    Console.WriteLine("Hace calor.");
+}
--- obj/Debug/net10.0/estructuras-control.AssemblyInfo.cs
+++ obj/Debug/net10.0/estructuras-control.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("estructuras-control")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc514fbf13bfb4982ca49ca515a56949ed98d98a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3ae1413bca32accb0c945ff194fca703008b80")]
 [assembly: System.Reflection.AssemblyProductAttribute("estructuras-control")]
 [assembly: System.Reflection.AssemblyTitleAttribute("estructuras-control")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
