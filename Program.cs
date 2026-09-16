a696f105b4c53b55781c6cb7b1efbfeeaa5d26fb
Juan <166779535+lardevpro@users.noreply.github.com>
2026-09-16 10:34:14 +0200

    Nuevo punto de recuperación
    Nuevo punto donde se puede recuperar todo lo hecho.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
     NIVEL 1 — if
     Ejercicio 1 — Mayor de edad
     Pide la edad de una persona.
@@ -7,7 +7,7 @@
 */
 
 Console.Write("Introduce tu edad:"); //manda un mensaje
-int? edad =  int.Parse(Console.ReadLine()); //recoge el nombre
+int? edad =  int.Parse(Console.ReadLine()!); //recoge el nombre
 
 if (edad >= 18)
 {
@@ -16,4 +16,32 @@ if (edad >= 18)
 else
 {
     Console.WriteLine("No es mayor de edad.");
+}
+    //*Ejercicio 2 — 
+    // Número positivo 
+    // Pide un número entero.Comprueba si es mayor que 0.
+
+Console.Write("Introduce un número entero:"); 
+int? numero = int.Parse(Console.ReadLine()!);
+
+if (numero > 0)
+{
+    Console.WriteLine("El número es positivo.");
+}
+else
+{
+    Console.WriteLine("El número no es positivo.");
+}
+
+    // Ejercicio 4 — Temperatura
+    // Pide una temperatura.
+    // Si es superior a 30 grados, muestra:
+    // Hace calor
+
+Console.Write("Introduce la temperatura:");
+int? temperatura = int.Parse(Console.ReadLine()!);
+
+if (temperatura > 30)
+{
+    Console.WriteLine("Hace calor.");
 }
