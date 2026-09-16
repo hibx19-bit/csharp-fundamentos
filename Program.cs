22808fb76107999d56c73af96c6f1be9298b4f86
hibx19-bit <hibx19@gmail.com>
2026-09-16 09:07:05 +0200

    NIVEL 1 — if

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!!!");
+﻿/*
+    NIVEL 1 — if
+    Ejercicio 1 — Mayor de edad
+    Pide la edad de una persona.
+    Si tiene 18 años o más, muestra:
+    Es mayor de edad
+*/
+
+Console.Write("Introduce tu edad:"); //manda un mensaje
+int? edad =  int.Parse(Console.ReadLine()); //recoge el nombre
+
+if (edad >= 18)
+{
+    Console.WriteLine("Es mayor de edad.");
+}
+else
+{
+    Console.WriteLine("No es mayor de edad.");
+}
